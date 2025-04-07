54230d9a89ce12333270b45c6f8663ab4400d2d7
Hjaltechristensen <hjalte-christensen@live.dk>
2025-04-07 15:09:08 +0200

    Mulighed for at kunne søge på categories

--- FoodWasteSalling/Shared/Models/Product.cs
+++ FoodWasteSalling/Shared/Models/Product.cs
@@ -6,5 +6,20 @@
 		public string? Description { get; set; }
 		public string? Ean { get; set; }
 		public string? Image { get; set; }
+		public List<string> SplitCategoryList { get; set; } = [];
+
+		public void SplitCategories()
+		{
+			if (Categories != null && Categories.ContainsKey("da"))
+			{
+				var raw = Categories["da"]?.ToString();
+				if (!string.IsNullOrEmpty(raw))
+				{
+					SplitCategoryList = raw.Split('>').Select(s => s.Trim()).ToList();
+				}
+			}
+		}
+
+
 	}
 }
